66a59390cf52cceb1ffc74eb2f631bc1b84dd5e8
SyedAdamJamal <syedadam509@gmail.com>
2021-02-19 02:48:03 +0500

    first_commit

---
+++ app/src/main/java/com/example/login_page/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.login_page
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+    }
+}
